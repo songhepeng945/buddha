5becde297f5617ff073e4160d63d0a6fead8b0e9
Song <1014786263@qq.com>
2018-11-21 14:50:20 +0800

    init servers

---
+++ src/server/BuddhaServer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xxx
+ * Date: 2018/11/21
+ * Time: 14:36
+ */
+
+namespace buddha\server;
+
+
+class BuddhaServer
+{
+    public function __construct()
+    {
+        var_dump("construct success");
+    }
+}
